dc38eab04611d452bc97501406ac251c24b2a0c1
Navin Rao <nunurao@gmail.com>
2015-02-09 23:49:43 -0500

    First commit

---
+++ src/EmployeeServlet.java
@@ -0,0 +1,11 @@
+/* Classes in the Java Resources folder are automatically complied and added to 
+ * the WEB-INF/classes folder*/
+
+public class EmployeeServlet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
